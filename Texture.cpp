c02e8d51000641ee2c0746663e8145f72b49d020
Blue <dinhletrieuduong@gmail.com>
2018-06-18 15:38:37 +0700

    Them comment trong createObject

--- Texture.cpp
+++ Texture.cpp
@@ -11,21 +11,31 @@ Texture::~Texture()
 }
 
 Screen* Texture::createObject(Screen* scr, int& i) {
-	scr->replaceChar(6 + i, 5, 219);
-	scr->replaceChar(7 + i, 5, 219);
-	scr->replaceChar(8 + i, 5, 219);
+	// Object di chuyen theo truc Ox voi delta x = 1, co the thay doi neu muon
 	scr->replaceChar(5 + i, 6, 219);
 	scr->replaceChar(5 + i, 7, 219);
+
+	scr->replaceChar(6 + i, 5, 219);
 	scr->replaceChar(6 + i, 6, 219);
 	scr->replaceChar(6 + i, 7, 219);
+
+	scr->replaceChar(7 + i, 5, 219);
 	scr->replaceChar(7 + i, 6, 219);
-	scr->replaceChar(7 + i, 7, 219);
+	scr->replaceChar(7 + i, 7, 219);	
+
+	scr->replaceChar(8 + i, 5, 219);
 	scr->replaceChar(8 + i, 6, 219);
 	scr->replaceChar(8 + i, 7, 219);
-	++i;
 
+	++i;
+	// Khi chay toi gan cuoi khung hinh` thi reset = 0 chay tu vi tri ban dau	
 	if (i == 110)
 		i = 0;
 
 	return scr;
+}
+
+bool Texture::checkAccident() {
+
+	return 1;
 }
