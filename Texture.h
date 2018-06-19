8498da9c588eba9a5a0f465894921848026c4c6e
dltd_blue <39703705+dinhletrieuduong@users.noreply.github.com>
2018-06-19 17:20:06 +0700

    thêm constructor với load file

--- Texture.h
+++ Texture.h
@@ -1,16 +1,22 @@
 #pragma once
-#include "Screen.h"
 #include <windows.h>
+#include <iostream>
+#include <string>
+#include "Screen.h"
+
+using namespace std;
 
 class Texture
 {
 private:
-
+	//Screen* m_screen;
+	float *m_x, *m_y, *m_color;
+	int m_countX, m_countY, m_countColor;
 public:
-	Texture();
+	Texture::Texture();
+	Texture(const Screen* scr);
 	~Texture();
 
+	bool load(string a);
 	void createObject(Screen* scr, int& i);
-	void create(Screen& scr, int& i);
 };
-
