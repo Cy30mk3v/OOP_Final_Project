877d2bf61b5cdbf3db8467d61ba97276b7de45bf
Cy30mk3v <40456349+Cy30mk3v@users.noreply.github.com>
2018-06-26 16:01:07 +0700

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -1,10 +1,10 @@
-﻿#include "Object.h"
+#include "Object.h"
 #include "Misc.h"
 
 Object::Object()
 {
-	this->H1.x = 0;
-
+	H1.x = 0;
+	H1.y = 0;
 }
 
 bool Object::isImpact(Object &E)
@@ -22,38 +22,80 @@ void Object::setObject(Point E)
 
 void Object::moveObject(int x, int y)
 {
-	Point P;
-	P.x = x;
-	P.y = y;
+
+	this->H1.x = x;
+	this->H1.y = y;
 }
 
 Point Object::valuePosition()
 {
-	
+	return H1;
+}
+
+Texture Object::getTexture()
+{
+	return H;
 }
 
 // Thời gian truyền vào được tính từ lúc bắt đầu chơi
+// Đèn đỏ thì dòng xe dừng và ngược lại khi đèn xanh
+// Dương sẽ lấy tín hiệu từ hàm này để vẽ Texture (nên để hàm này trong File Texture)
 bool statusSignal(int time)
 {
-	return(time < rand() % 60 + 1);
+	if(time < rand() % 60 + 1);
+	{
+		return RED;
+	}
+	return GREEN;
 }
 
-void Object::drawObjects(int n)
+// Từ đây, n = số lượng Object
+void drawObjects(Object *E,int n)
 {
-	Object P;
-	P.H = new Texture[n];
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			P[i].H[j].draw;
-		}
+		E[i].getTexture.draw(E[i].valuePosition);
 	}
 }
 
+// max_y là vị trí của hàng object cao nhất +1
+bool Human::isFinish(Object *E, int n, int max_y)
+{
+	if(this->valuePosition().y == max_y && this->isImpact(E,n)==false);
+	{
+		this->mState = ALIVE;
+		return ALIVE;
+	}
+}
 
-void 
+bool Human::isDead(Object *E, int n)
+{
+	if (this->isImpact(E, n))
+	{
+		this->mState = DEAD;
+		return DEAD;
+	}
+	return ALIVE;
+}
 
+bool Human::isImpact(Object *E, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (E[i].valuePosition.x == this->valuePosition.x && E[i].valuePosition.y == this->valuePosition.y)
+		{
+			this->mState = DEAD;
+			return DEAD;
+		}
+	}
+	this->mState = ALIVE;
+	return ALIVE;
+}
+
+void Human::drawHuman()
+{
+	this->getTexture().draw(this->valuePosition());
+}
 
 
 
